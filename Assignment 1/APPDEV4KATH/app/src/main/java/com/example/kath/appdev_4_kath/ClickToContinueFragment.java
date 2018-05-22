8104122886925b317c3997c7957314e566c7e11d
Riccardo Tiebax <riccardo.t@gmail.com>
2018-05-22 20:24:44 +0200

    Code review on Assignment 1 by Katharina.

--- Assignment 1/APPDEV4KATH/app/src/main/java/com/example/kath/appdev_4_kath/ClickToContinueFragment.java
+++ Assignment 1/APPDEV4KATH/app/src/main/java/com/example/kath/appdev_4_kath/ClickToContinueFragment.java
@@ -60,6 +60,13 @@ public class ClickToContinueFragment extends Fragment {
        mailIntent.putExtra(Intent.EXTRA_SUBJECT, "Hello World!");
        mailIntent.putExtra(Intent.EXTRA_TEXT, "I did it! Greetings, Kath");
 
+		// If there is no specific
+		// activity to handle the intent,
+		// The below call will result in an exception.
+		// Now, for calls this is most likely no issue,
+		// but I'd check this anyway to be sure using
+		// (mailIntent.resolveActivity(getPackageManager()) != null)
+		// The same holds for the the call intent below. 
        startActivity(mailIntent);
     }
 
@@ -68,7 +75,13 @@ public class ClickToContinueFragment extends Fragment {
 
         Uri phoneNumber = Uri.parse("tel:040 782 0814");
         Intent callIntent = new Intent(Intent.ACTION_DIAL, phoneNumber);
+		// Formally you would need to have
+		// the permission android.Manifest.permission.CALL_PHONE
+		// granted by the time you start the intent
 
+		// In reference to this, I also placed a comment
+		// in the manifest, in which you would have to
+		// add that permission as well.
         startActivity(callIntent);
     }
 
